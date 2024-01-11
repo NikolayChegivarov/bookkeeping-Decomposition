a916f430aa8cd7f3e90ddefa2975649f9827af58
Nikolai Gusarov <Nikolai_polos@mail.ru>
2024-01-11 15:35:12 +0300

    initial first commit

--- application/db/people.py
+++ application/db/people.py
@@ -1,5 +1,6 @@
 def get_employees():
     print('Модуль people.py функция get_employees()')
+    
 
 if __name__ == '__main__':
     get_employees()
--- application/salary.py
+++ application/salary.py
@@ -2,7 +2,7 @@
 def calculate_salary():
     print('Модуль salary.py, функция calculate_salary()')
 
-
+if __name__ == '__main__':
     calculate_salary()
 else:
     print("Модуль содержит конструкцию __name__ == '__main__'")
